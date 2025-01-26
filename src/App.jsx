fa62d475fa4d258f33e54d58b8caf928520b2daf
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-01-26 10:19:09 +0100

    formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import NavBar from './components/NavBar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListings from './components/JobListings'
-import ViewAllJobs from './components/ViewAllJobs'
+import React from "react";
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
 
 export default function App() {
   return (
     <>
-    <NavBar />
-    <Hero />
-    <HomeCards />
-    <JobListings />
-    <ViewAllJobs />
+      <NavBar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
+      <ViewAllJobs />
     </>
-  )
+  );
 }
