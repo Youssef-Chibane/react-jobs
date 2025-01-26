94e4a6c72c16754a3b00b2ff1002e325259d3d2f
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-01-26 11:52:22 +0100

    add layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,19 @@ import {
   RouterProvider,
   Route,
 } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route index element={<HomePage />} />
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+    </Route>
   )
 );
 
 const App = () => {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 };
 
 export default App;
