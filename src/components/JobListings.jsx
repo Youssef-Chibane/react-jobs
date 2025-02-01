bfbce320d45217738083d5b27fee81728d2233df
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-02-01 15:46:29 +0100

    Spinner

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import JobListing from "./JobListing";
+import Spinner from "./Spinner";
 
 const JobListings = ({ isHome = false }) => {
   const [jobs, setJobs] = useState([]);
@@ -18,20 +19,24 @@ const JobListings = ({ isHome = false }) => {
       }
     };
 
-    fetchJobs(); // Call the function here
+    fetchJobs();
   }, []);
 
   return (
-    <section class="bg-blue-50 px-4 py-10">
-      <div class="container-xl lg:container m-auto">
-        <h2 class="text-3xl font-bold text-indigo-500 mb-6 text-center">
+    <section className="bg-blue-50 px-4 py-10">
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           {isHome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {jobs.map((job) => (
-            <JobListing key={job.id} job={job} />
-          ))}
-        </div>
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs.map((job) => (
+              <JobListing key={job.id} job={job} />
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
