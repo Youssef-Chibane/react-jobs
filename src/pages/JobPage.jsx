b379961aefd5a2810c27b27cf8a05d0cc6608b6b
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-02-03 03:08:46 +0100

    single job fetching

---
+++ src/pages/JobPage.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import Spinner from "../components/Spinner";
+import { useParams } from "react-router-dom";
+
+const JobPage = () => {
+  const { id } = useParams();
+  const [job, setJob] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJob = async () => {
+      try {
+        const res = await fetch(`http://localhost:3000/jobs/${id}`);
+        const data = await res.json();
+        setJob(data);
+      } catch (error) {
+        console.error("Error fetching data", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchJob();
+  }, []);
+
+  return loading ? <Spinner /> : <h1>{job.title}</h1>;
+};
+
+export default JobPage;
