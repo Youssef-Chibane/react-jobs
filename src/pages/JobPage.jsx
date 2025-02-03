7dc12c711e966bed59bb8144df09c5c8fc6a888b
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-02-03 03:21:15 +0100

    job loader

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,29 +1,42 @@
 import { useState, useEffect } from "react";
 import Spinner from "../components/Spinner";
-import { useParams } from "react-router-dom";
+import { useParams, useLoaderData } from "react-router-dom";
+
+// const JobPage = () => {
+//   const { id } = useParams();
+//   const [job, setJob] = useState(null);
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     const fetchJob = async () => {
+//       try {
+//         const res = await fetch(`http://localhost:3000/jobs/${id}`);
+//         const data = await res.json();
+//         setJob(data);
+//       } catch (error) {
+//         console.error("Error fetching data", error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+
+//     fetchJob();
+//   }, []);
+
+//   return loading ? <Spinner /> : <h1>{job.title}</h1>;
+// };
 
 const JobPage = () => {
-  const { id } = useParams();
-  const [job, setJob] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const job = useLoaderData();
 
-  useEffect(() => {
-    const fetchJob = async () => {
-      try {
-        const res = await fetch(`http://localhost:3000/jobs/${id}`);
-        const data = await res.json();
-        setJob(data);
-      } catch (error) {
-        console.error("Error fetching data", error);
-      } finally {
-        setLoading(false);
-      }
-    };
 
-    fetchJob();
-  }, []);
-
-  return loading ? <Spinner /> : <h1>{job.title}</h1>;
+  return <div>{job.title}</div>;
 };
 
-export default JobPage;
+const jobLoader = async ({ params }) => {
+  const res = await fetch(`http://localhost:3000/jobs/${params.id}`);
+  const data = await res.json();
+  return data;
+};
+
+export { JobPage as default, jobLoader };
