dd3c30b97f8610ae1bccf1117339b9a560961b5a
Youssef-Chibane <chib4ne.youssef@gmail.com>
2025-02-12 14:28:19 +0100

    cleaning

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,33 +1,7 @@
-import { useState, useEffect } from "react";
-import Spinner from "../components/Spinner";
 import { useParams, useLoaderData, useNavigate } from "react-router-dom";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
-
-// const JobPage = () => {
-//   const { id } = useParams();
-//   const [job, setJob] = useState(null);
-//   const [loading, setLoading] = useState(true);
-
-//   useEffect(() => {
-//     const fetchJob = async () => {
-//       try {
-//         const res = await fetch(`http://localhost:3000/jobs/${id}`);
-//         const data = await res.json();
-//         setJob(data);
-//       } catch (error) {
-//         console.error("Error fetching data", error);
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-
-//     fetchJob();
-//   }, []);
-
-//   return loading ? <Spinner /> : <h1>{job.title}</h1>;
-// };
 
 const JobPage = ({ deleteJob }) => {
   const job = useLoaderData();
@@ -112,7 +86,7 @@ const JobPage = ({ deleteJob }) => {
               <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                 <h3 className="text-xl font-bold mb-6">Manage Job</h3>
                 <Link
-                  to={`/jobs/edit/${job.id}`}
+                  to={`/edit-job/${job.id}`}
                   className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >
                   Edit Job
